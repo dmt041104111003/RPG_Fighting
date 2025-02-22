fba909eb14832ab342e750bda0e2b28d4307be11
dmt041104111003 <daomanhtung4102003@gmail.com @example.com>
2025-02-22 19:56:44 +0700

    Debug (back)

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,75 +5,62 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     public GameObject[] attackPrefabs;
-    private FighterStats fighterStats;
-    private float attackCooldown = 3f; 
+    private FighterAction fighterAction;
+    private float attackCooldown = 3f;
     private float attackCooldownTimer = 0f;
-    private int attackTypeIndex;
+    private int attackTypeIndex = 0; 
     private GameObject hero;
     private Vector3 originalPosition;
-    public float moveSpeed = 5f;
     public float meleeDistance = 1.5f;
-    private bool isAttacking;
+
     void Start()
     {
-        fighterStats = GetComponent<FighterStats>();
+        fighterAction = GetComponent<FighterAction>();
         hero = GameObject.FindGameObjectWithTag("Hero");
-        originalPosition = transform.position;
+        originalPosition = transform.position; 
         attackCooldownTimer = attackCooldown;
-        attackTypeIndex = 0;
-        isAttacking = false;
     }
 
     void Update()
     {
-
-
-
         attackCooldownTimer += Time.deltaTime;
- 
+
         if (attackCooldownTimer >= attackCooldown)
         {
-
             PerformAttack();
             attackCooldownTimer = 0f;
-   isAttacking = false;
         }
     }
 
     void PerformAttack()
     {
+        if (attackPrefabs.Length > 0)
+        {
+            GameObject attackPrefab = attackPrefabs[attackTypeIndex];
+            if (attackPrefab != null)
+            {
+                AttackScript attackScript = attackPrefab.GetComponent<AttackScript>();
+                float magicCost = attackScript.magicCost;
+                bool magicAttack = attackScript.magicAttack;
+                FighterStats grantEnemyStats = GetComponent<FighterStats>();
 
-        if (attackPrefabs.Length == 0 || isAttacking) return;
-
-        isAttacking = true;
-        float distanceToHero = Vector3.Distance(transform.position, hero.transform.position);
-        GameObject attackPrefab = attackPrefabs[attackTypeIndex];
-        AttackScript attackScript = attackPrefab.GetComponent<AttackScript>();
-
-        if (distanceToHero <= 5f && fighterStats.magic >= attackPrefabs[0].GetComponent<AttackScript>().magicCost)
-        {
-            attackPrefab = attackPrefabs[0];
-            attackTypeIndex = 0;
-        }
-        else if (fighterStats.magic >= attackPrefabs[1].GetComponent<AttackScript>().magicCost)
-        {
-            attackPrefab = attackPrefabs[1];
-            attackTypeIndex = 1;
-        }
-        else
-        {
-            isAttacking = false;
-            return; 
-        }
-
-        if (attackTypeIndex == 0)
-        {
-            StartCoroutine(MeleeAttackSequence(attackScript, hero));
-        }
-        else
-        {
-            attackScript.Attack(hero);
-            isAttacking = false;
+                if (grantEnemyStats.magic >= magicCost)
+                {
+                    if (attackTypeIndex == 0)
+                    {
+                        StartCoroutine(MeleeAttackSequence(attackScript, hero));
+                    }
+                    else if (attackTypeIndex == 1) 
+                    {
+                        attackScript.Attack(hero);
+                    }
+                    attackTypeIndex = (attackTypeIndex + 1) % 2;
+                }
+                else
+                {
+                    attackTypeIndex = (attackTypeIndex + 1) % 2;
+                }
+            }
         }
     }
 
@@ -83,12 +70,11 @@ public class EnemyAI : MonoBehaviour
         Vector3 targetPosition = target.transform.position;
         Vector3 direction = (targetPosition - transform.position).normalized;
         Vector3 meleePosition = targetPosition - direction * meleeDistance;
-        transform.position = meleePosition;
+        transform.position = meleePosition; 
+
         attackScript.Attack(target);
-        yield return new WaitForSeconds(1f); 
+        yield return new WaitForSeconds(1f);
 
         transform.position = originalPosition;
-        isAttacking = false;
     }
-
 }
